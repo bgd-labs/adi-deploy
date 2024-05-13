8c2f8970e00088a40338c7d62d9b6317f6e1c885
sendra <polsendra@gmail.com>
2024-05-13 18:37:34 +0200

    fix: verification working

--- scripts/payloads/Deploy_ArbAdapter_Payload.s.sol
+++ scripts/payloads/Deploy_ArbAdapter_Payload.s.sol
@@ -1,40 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import {SimpleReceiverAdapterUpdate} from 'aave-helpers/adi/SimpleReceiverAdapterUpdate.sol';
 import {ArbAdapterDeploymentHelper, BaseAdapterStructs} from 'adi-scripts/Adapters/DeployArbAdapter.sol';
 import {TestNetChainIds} from 'adi-scripts/contract_extensions/TestNetChainIds.sol';
 import {IBaseAdapter} from 'adi/adapters/IBaseAdapter.sol';
 import {DeployArbAdapter} from '../adapters/DeployArbAdapter.s.sol';
 import {Create2Utils} from 'aave-helpers/ScriptUtils.sol';
+import {AaveV3Arbitrum_New_Adapter_Payload} from './ArbAdapterPayload.sol';
 
 //import {ChainIds} from 'aave-helpers/ChainIds.sol';
-
-/**
- * @title Arbitrum bridge adapter update example
- * @author BGD Labs @bgdlabs
- * - Discussion: https://governance.aave.com/t/bgd-technical-maintenance-proposals/15274/31
- */
-contract AaveV3Arbitrum_New_Adapter_Payload is SimpleReceiverAdapterUpdate {
-  constructor(
-    address crossChainController,
-    address newAdapter
-  )
-    SimpleReceiverAdapterUpdate(
-      SimpleReceiverAdapterUpdate.ConstructorInput({
-        ccc: crossChainController,
-        adapterToRemove: address(0),
-        newAdapter: newAdapter
-      })
-    )
-  {}
-
-  function getChainsToReceive() public pure override returns (uint256[] memory) {
-    uint256[] memory chains = new uint256[](1);
-    chains[0] = TestNetChainIds.ETHEREUM_SEPOLIA;
-    return chains;
-  }
-}
 
 /**
  * @dev Deploy Arbitrum
