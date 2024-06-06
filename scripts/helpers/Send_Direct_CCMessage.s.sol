174185c35f0b5069e74e2e74384a0e7da18ae2b9
sendra <polsendra@gmail.com>
2024-06-06 20:29:28 +0200

    fix: added helper scripts

---
+++ scripts/helpers/Send_Direct_CCMessage.s.sol
@@ -0,0 +1,92 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import '../BaseDeployerScript.sol';
+import {ICrossChainForwarder} from 'adi/interfaces/ICrossChainForwarder.sol';
+
+abstract contract BaseSendDirectMessage is BaseDeployerScript {
+  function DESTINATION_NETWORK() public view virtual returns (uint256);
+
+  function getDestinationAddress() public view virtual returns (address) {
+    return _getAddresses(DESTINATION_NETWORK()).mockDestination;
+  }
+
+  function getGasLimit() public view virtual returns (uint256) {
+    return 150_000;
+  }
+
+  function getMessage() public view virtual returns (bytes memory) {
+    return abi.encode('some random message');
+  }
+
+  function _execute(DeployerHelpers.Addresses memory addresses) internal override {
+    uint256 destinationChainId = _getAddresses(DESTINATION_NETWORK()).chainId;
+
+    ICrossChainForwarder(addresses.crossChainController).forwardMessage(
+      destinationChainId,
+      getDestinationAddress(),
+      getGasLimit(),
+      getMessage()
+    );
+  }
+}
+
+contract Ethereum is BaseSendDirectMessage {
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.ETHEREUM;
+  }
+
+  function DESTINATION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.METIS;
+  }
+}
+
+contract Avalanche is BaseSendDirectMessage {
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.AVALANCHE;
+  }
+
+  function DESTINATION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.ETHEREUM;
+  }
+}
+
+contract Polygon is BaseSendDirectMessage {
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.POLYGON;
+  }
+
+  function DESTINATION_NETWORK() public pure override returns (uint256) {
+    return ChainIds.ETHEREUM;
+  }
+}
+
+contract Ethereum_testnet is BaseSendDirectMessage {
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.ETHEREUM_SEPOLIA;
+  }
+
+  function DESTINATION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.POLYGON_AMOY;
+  }
+}
+
+contract Avalanche_testnet is BaseSendDirectMessage {
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.AVALANCHE_FUJI;
+  }
+
+  function DESTINATION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.ETHEREUM_SEPOLIA;
+  }
+}
+
+contract Polygon_testnet is BaseSendDirectMessage {
+  function TRANSACTION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.POLYGON_AMOY;
+  }
+
+  function DESTINATION_NETWORK() public pure override returns (uint256) {
+    return TestNetChainIds.ETHEREUM_SEPOLIA;
+  }
+}
