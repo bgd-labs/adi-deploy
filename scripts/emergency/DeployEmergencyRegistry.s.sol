c9d89cf5e4c0536e6eb6a6d31f51523b9182d49b
sendra <polsendra@gmail.com>
2024-06-06 19:46:33 +0200

    feat: emregency registry script

---
+++ scripts/emergency/DeployEmergencyRegistry.s.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: BUSL-1.1
+pragma solidity ^0.8.0;
+
+import '../BaseDeployerScript.sol';
+import 'adi-scripts/emergency/Deploy_EmergencyRegistry.sol';
+
+abstract contract DeployEmergencyRegistry is BaseDeployerScript, BaseDeployEmergencyMode {
+  function _execute(Addresses memory addresses) internal override {
+    addresses.emergencyRegistry = _deployEmergencyRegistry();
+    Ownable(address(addresses.emergencyRegistry)).transferOwnership(OWNER());
+  }
+}
+
+contract Ethereum is DeployEmergencyRegistry {
+  function TRANSACTION_NETWORK() internal pure override returns (uint256) {
+    return ChainIds.ETHEREUM;
+  }
+}
+
+contract Ethereum_testnet is DeployEmergencyRegistry {
+  function TRANSACTION_NETWORK() internal pure override returns (uint256) {
+    return TestNetChainIds.ETHEREUM_SEPOLIA;
+  }
+}
