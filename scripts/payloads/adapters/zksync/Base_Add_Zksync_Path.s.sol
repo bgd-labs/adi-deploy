5ed4e96f9744cf1e7011d9f1b0ee413151cde541
sendra <polsendra@gmail.com>
2024-07-26 13:20:44 +0200

    fix: use create2 in correct adapter

--- scripts/payloads/adapters/zksync/Base_Add_Zksync_Path.s.sol
+++ scripts/payloads/adapters/zksync/Base_Add_Zksync_Path.s.sol
@@ -22,13 +22,13 @@ abstract contract Base_Deploy_Add_ZkSync_Path_Payload is BaseDeployerScript {
   function _getAdapterAddress(address crossChainController) internal virtual returns (address);
 
   function _execute(Addresses memory addresses) internal virtual override {
-    //    Addresses memory destinationAddresses = _getAddresses(DESTINATION_CHAIN_ID());
+    Addresses memory destinationAddresses = _getAddresses(DESTINATION_CHAIN_ID());
 
     _deployPayload(
       AddForwarderAdapterArgs({
         crossChainController: addresses.crossChainController,
-        currentChainBridgeAdapter: addresses.zksyncAdapter,
-        destinationChainBridgeAdapter: _getAdapterAddress(addresses.crossChainController), //destinationAddresses.zksyncAdapter,
+        currentChainBridgeAdapter: _getAdapterAddress(addresses.crossChainController), //addresses.zksyncAdapter,
+        destinationChainBridgeAdapter: destinationAddresses.zksyncAdapter,
         destinationChainId: DESTINATION_CHAIN_ID()
       })
     );
